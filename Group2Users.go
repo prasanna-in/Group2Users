0e1a952e332488713168b1b64145e17b3009a656
Prasanna Kanagasabai <prasanna.in@gmail.com>
2017-07-06 11:19:16 +0800

    Changed the group2Users to be aLibrary

--- Group2Users.go
+++ Group2Users.go
@@ -1,42 +1,32 @@
-package main
+package group2users
 
 import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"log"
 	"github.com/aws/aws-sdk-go/aws"
-	"os"
-	"fmt"
+
 )
 
-func main() {
-	lookupGroup, users := getUsers(os.Args[1])
-	fmt.Println("Users in Group "+lookupGroup+" : ", users)
-}
-
-func getUsers(GroupName string) (string, []string) {
-	lookupGroup := GroupName
-	var users []string
-	iamService := getIAMService()
-	
-	resultUsers, err := iamService.ListUsers(&iam.ListUsersInput{})
-	for _, value := range resultUsers.Users {
-		groupsOutput, _ := iamService.ListGroupsForUser(&iam.ListGroupsForUserInput{UserName: value.UserName})
-		for _, val := range groupsOutput.Groups {
-			if aws.StringValue(val.GroupName) == lookupGroup {
-				users = append(users, aws.StringValue(value.UserName))
+func ListUsersForGroup(iamService *iam.IAM,groupName string)  (string, []string) {
+	lookupGroup := groupName
+		var users []string
+		resultUsers, err := iamService.ListUsers(&iam.ListUsersInput{})
+		for _, value := range resultUsers.Users {
+			groupsOutput, _ := iamService.ListGroupsForUser(&iam.ListGroupsForUserInput{UserName: value.UserName})
+			for _, val := range groupsOutput.Groups {
+				if aws.StringValue(val.GroupName) == lookupGroup {
+					users = append(users, aws.StringValue(value.UserName))
+				}
 			}
 		}
-	}
-	if err != nil {
-		log.Fatal("error", err.Error())
-	}
-	return lookupGroup, users
+		if err != nil {
+			log.Fatal("error", err.Error())
+		}
+		return lookupGroup, users
+	
 }
-func getIAMService() *iam.IAM {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	iamService := iam.New(sess)
-	return iamService
+
+func GetIAMService(session *session.Session) (*iam.IAM) {
+	return iam.New(session)
 }
